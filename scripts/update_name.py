c4510ee57158cfebb657cad230a0c01d9be86cb4
jwlodek <jwlodek@cs.stonybrook.edu>
2019-06-24 10:22:31 -0400

    minor changes to the update name script

--- scripts/update_name.py
+++ scripts/update_name.py
@@ -1,23 +1,34 @@
-# Python script used to replace the name of the plugin
-# whereever applicable to simplify using the template.
-# The script can be run with:
-#
-# python3 update_name.py -n NAME
-#
-# where NAME is the name of the plugin you are developing
-#
+"""
+Python script used to replace the name of the plugin
+whereever applicable to simplify using the template.
+The script can be run with:
+python3 update_name.py -n NAME
+
+where NAME is the name of the plugin you are developing
+"""
+
+__author__      = "Jakub Wlodek"
+__copyright__   = "Copyright June 2019, Brookhaven Science Associates"
+__license__     = "GPL"
+__version__     = "R0.4"
+__maintainer__  = "Jakub Wlodek"
+
 
 import os
 import argparse
+from sys import platform
 
 
-# Updates the name of the main plugin directory
 def update_dir_names(all_lowercase):
+    """ Updates the name of the main plugin directory """
+
     if os.path.exists("../pluginApp"):
         os.rename("../pluginApp", "../"+all_lowercase+"App")
 
-# Updates the filenames of all of the plugin specific files. Also updates the contents of all files that are not in the src dir
+
 def update_file_names(all_lowercase, standard_name):
+    """ Updates the filenames of all of the plugin specific files. Also updates the contents of all files that are not in the src dir """
+
     if os.path.exists("../"+all_lowercase+"App/src/NDPlugin___.cpp"):
         os.rename("../"+all_lowercase+"App/src/NDPlugin___.cpp", "../"+all_lowercase+"App/src/NDPlugin"+standard_name+".cpp")
     if os.path.exists("../"+all_lowercase+"App/src/NDPlugin___.h"):
@@ -39,8 +50,10 @@ def update_file_names(all_lowercase, standard_name):
     if(os.path.exists("add_pv.py")):
         update_source_file(all_lowercase, all_lowercase.upper(), standard_name, "add_pv.py")
 
-# Reads file line by line and updates specific locations with correct plugin name
+
 def update_source_file(all_lowercase, all_uppercase, standard_name, file_path):
+    """ Reads file line by line and updates specific locations with correct plugin name """
+
     os.rename(file_path, file_path+"_OLD")
     oldFile = open(file_path+"_OLD", "r")
     newFile = open(file_path, "w")
@@ -61,8 +74,9 @@ def update_source_file(all_lowercase, all_uppercase, standard_name, file_path):
     os.remove(file_path+"_OLD")
 
 
-# Updates all of the files located in the src dir
 def update_sources(all_lowercase, all_uppercase, standard_name):
+    """ Updates all of the files located in the src dir """
+
     if os.path.exists("../"+all_lowercase+"App/src"):
         src_dir = "../"+all_lowercase+"App/src"
         for file in os.listdir(src_dir):
@@ -70,17 +84,17 @@ def update_sources(all_lowercase, all_uppercase, standard_name):
                 update_source_file(all_lowercase, all_uppercase, standard_name, src_dir + "/" + file)
 
 
-# Renames the plugin root directory to reflect the new plugin name
 def update_root_dir(standard_name):
+    """ Renames the plugin root directory to reflect the new plugin name """
+
     for directory_name in os.listdir("../.."):
         if "ADPluginTemplate" in directory_name:
             os.rename("../../"+directory_name, "../../ADPlugin"+standard_name)
 
 
-
-
-# Parses name from user args in command line
 def parse_args():
+    """ Parses name from user args in command line - no longer used """
+
     parser = argparse.ArgumentParser(description="Update plugin names in template")
     parser.add_argument('-n', '--name', help = 'Name of the plugin')
     arguments = vars(parser.parse_args())
@@ -104,6 +118,8 @@ def parse_args():
 #parse_args()
 
 def run_all(name):
+    """ updates names """
+
     all_lowercase = name.lower()
     all_uppercase = name.upper()
     standard_name = all_lowercase.capitalize()
@@ -111,4 +127,5 @@ def run_all(name):
     update_dir_names(all_lowercase)
     update_file_names(all_lowercase, standard_name)
     update_sources(all_lowercase, all_uppercase, standard_name)
-    update_root_dir(standard_name)
+    if platfrom == 'linux':
+        update_root_dir(standard_name)
