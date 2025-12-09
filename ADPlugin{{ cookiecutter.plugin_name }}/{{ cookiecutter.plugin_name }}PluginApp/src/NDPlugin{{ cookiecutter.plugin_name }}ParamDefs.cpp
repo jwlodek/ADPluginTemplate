3faaf9d77e60fee5f23cc3ec8ea156a017ccda59
Jakub Wlodek <jwlodek@bnl.gov>
2025-12-09 10:44:29 -0500

    Another fix

--- ADPlugin{{ cookiecutter.plugin_name }}/{{ cookiecutter.plugin_name }}PluginApp/src/NDPlugin{{ cookiecutter.plugin_name }}ParamDefs.cpp
+++ ADPlugin{{ cookiecutter.plugin_name }}/{{ cookiecutter.plugin_name }}PluginApp/src/NDPlugin{{ cookiecutter.plugin_name }}ParamDefs.cpp
@@ -1,3 +1,5 @@
+#include "NDPlugin{{ cookiecutter.plugin_name }}.h"
+
 void NDPlugin{{ cookiecutter.plugin_name }}::createAllParams(){
     // This function is called during the plugin initialization to create
     // all the parameters defined for this plugin.
